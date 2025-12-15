f249c2f3b14f30efb1fb0c3a2ea4d1c3ccf256b9
水橋パルスィ <git@xyk.one>
2025-12-15 10:17:12 +0900

    fix

--- hikari_bot/plugins/mycard_subscriber.py
+++ hikari_bot/plugins/mycard_subscriber.py
@@ -5,7 +5,7 @@ from nonebot import get_driver, logger
 from nonebot.adapters.onebot.v11 import Bot
 from hikari_bot.utils.constants import *
 from hikari_bot.utils.mycard import *
-from hikari_bot.plugins.common import *
+from hikari_bot.plugins.common import message_superusers
 
 _stop_event = asyncio.Event()
 
